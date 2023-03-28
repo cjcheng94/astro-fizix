50c5e409de9c4796b1a187e08c4d465a8953c53e
Alex Cheng <alexcheng94@126.com>
2023-03-28 20:32:03 +0800

    💄 added mono font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem"
+        }
+      },
+      fontFamily: {
+        mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono]
+      }
+    }
+  },
+  plugins: []
+};
